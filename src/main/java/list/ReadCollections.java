ace4047c609e1814a73d8bb6d8fb95e73f2c295d
drinktea <null>
2019-09-05 17:01:03 +0800

    新建项目

---
+++ src/main/java/list/ReadCollections.java
@@ -0,0 +1,10 @@
+package list;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class ReadCollections {
+    public static void main(String[] args) {
+//        Collections
+    }
+}
