55aa146b1dc5e48b60ed8852e35d47b5fa14869b
drinktea <null>
2019-09-10 21:52:38 +0800

    添加spring源码阅读的内容

---
+++ coderead/src/main/java/coderead/spring/CodereadApplication.java
@@ -0,0 +1,13 @@
+package coderead.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodereadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodereadApplication.class, args);
+    }
+
+}
