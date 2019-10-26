437a8f988940581b15407d7f653146d6404b45d0
drinktea <null>
2019-10-26 12:25:51 +0800

    spring差不多了

--- coderead/src/main/java/coderead/spring/controller/MyBatisController.java
+++ coderead/src/main/java/coderead/spring/controller/MyBatisController.java
@@ -3,17 +3,25 @@ package coderead.spring.controller;
 import coderead.spring.mybaits.entity.User;
 import coderead.spring.mybaits.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 @RestController
 public class MyBatisController {
     @Autowired
     private UserService userService;
 
     @GetMapping("/users")
+    @Transactional(rollbackForClassName = "Exception.class")
     public List<User> lists() {
+          User user = new User();
+          user.setUserName("jimmy");
+          user.setUserAge(13);
+          userService.insertUser(user);
+          userService.insertUser(new User());
         return userService.getUsers();
     }
 }
