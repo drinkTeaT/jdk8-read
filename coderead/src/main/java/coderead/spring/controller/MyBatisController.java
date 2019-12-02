d9ecab5fd5bce2758153e4ecddd68e902ab11f89
liuweibin <liuweibin@linklogis.com>
2019-12-02 18:07:33 +0800

    controller参数

--- coderead/src/main/java/coderead/spring/controller/MyBatisController.java
+++ coderead/src/main/java/coderead/spring/controller/MyBatisController.java
@@ -1,13 +1,12 @@
 package coderead.spring.controller;
 
+import coderead.spring.entity.Person;
 import coderead.spring.mybaits.entity.User;
 import coderead.spring.mybaits.service.MyService;
 import coderead.spring.mybaits.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,10 +21,10 @@ public class MyBatisController {
     @GetMapping("/users")
     @Transactional(rollbackForClassName = "Exception.class")
     public List<User> lists() {
-          User user = new User();
-          user.setUserName("jimmy");
-          user.setUserAge(13);
-          userService.insertUser(user);
+        User user = new User();
+        user.setUserName("jimmy");
+        user.setUserAge(13);
+        userService.insertUser(user);
 //          userService.insertUser(new User());
         return userService.getUsers();
     }
@@ -34,5 +33,15 @@ public class MyBatisController {
     public List<User> queryPage() {
         List<User> result = service.queryUser(1, 2);
         return result;
+    }
+
+    @RequestMapping("/")
+    public String getStringFromWeb(@RequestParam String userId, @RequestParam String phone) {
+        return userId + " " + phone;
+    }
+
+    @RequestMapping("/entity")
+    public String getEntityFromWeb(@RequestBody Person person){
+        return person.toString();
     }
 }
---
+++ coderead/src/main/java/coderead/spring/entity/Person.java
@@ -0,0 +1,18 @@
+package coderead.spring.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Description
+ * Author liuweibin
+ * Date 2019/12/2 17:59
+ **/
+@Setter
+@Getter
+@ToString
+public class Person {
+    private String id;
+    private String phone;
+}
